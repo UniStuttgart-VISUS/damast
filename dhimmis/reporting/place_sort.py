2a8e4de4fe100a7a424e71721ca7fd6d2b34af81
Max Franke <Max.Franke@vis.uni-stuttgart.de>
2022-03-10 09:41:28 +0100

    update which characters to ignore for report place name sorting
    re #94

--- dhimmis/reporting/place_sort.py
+++ dhimmis/reporting/place_sort.py
@@ -2,7 +2,15 @@ import re
 import locale
 from functools import cmp_to_key
 
-_ignore_name_prefixes = re.compile("^(a([tdrzsṣḍṭẓln]|[tds]h)-[’']?|[’'])")
+# characters to ignore before the sortable name, including apostrophe-like characters
+
+_apostrophe_like = ''.join((
+        '\'',        # U+0027 APOSTROPHE [used in simple transcription for both ʿain and hamza]
+        '\u02BF',    # U+02BF ʿ MODIFIER LETTER LEFT HALF RING [used in scientific transcription for ʿain]
+        '\u02BE',    # U+02BE ʾ MODIFIER LETTER RIGHT HALF RING [used in scientific transcription for hamza]
+        ))
+
+_ignore_name_prefixes = re.compile(F"^(a([tdrzsṣḍṭẓln]|[tds]h)-[{_apostrophe_like}]?|[{_apostrophe_like}])")
 
 def core_name(name):
     return _ignore_name_prefixes.sub('', name)
@@ -21,3 +29,4 @@ def sort_alternative_placenames(alt):
 
     return sorted(alt,
             key=lambda a: (comp(a.language), comp(a.transcription if a.transcription is not None else a.name)))
+
